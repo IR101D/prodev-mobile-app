300a6a27bbb43b00383257138ed128169b42c8ef
IR101D <somefkingadress@gmail.com>
2025-11-09 18:31:58 +0100

    Update _layout.tsx

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -5,7 +5,9 @@ export default function RootLayout() {
     <Stack screenOptions={{
       headerShown: false,
     }}>
-      
+      <Stack.Screen name="/index" />
+      <Stack.Screen name="join" />
+      <Stack.Screen name="signin" />
         <Tabs.Screen name="profile" options={{
         title: 'Profile',
          headerShown: false,
